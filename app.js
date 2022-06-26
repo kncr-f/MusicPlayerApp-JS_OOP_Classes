ddbf70d92e89dfae57b8909381d24fedb44c120f
Fatih Kincir <kincirfatih@gmail.com>
2022-06-26 22:44:23 +0200

    controls button functionality added

--- app.js
+++ app.js
@@ -3,8 +3,8 @@ const image = document.querySelector("#music-image");
 // const audio = document.querySelector("#audio");
 const title = document.querySelector("#music-details .title");
 const singer = document.querySelector("#music-details .singer");
-const prev = document.querySelector("#controls #prev");
 const play = document.querySelector("#controls #play");
+const prev = document.querySelector("#controls #prev");
 const next = document.querySelector("#controls #next");
 
 const player = new MusicPlayer(musicList);
@@ -26,9 +26,53 @@ function displayMusic(music) {
 
 }
 
-play.addEventListener("click", () => {
-    audio.play()
-})
 
+play.addEventListener("click", () => {
+    const isMusicPlay = container.classList.contains("playing");
+    isMusicPlay ? pauseMusic() : playMusic();
+    togglePlay();
+
+});
+
+prev.addEventListener("click", () => {
+    prevMusic();
+});
+
+next.addEventListener("click", () => {
+    nextMusic();
+});
+
+function prevMusic() {
+    player.previous();
+    let music = player.getMusic();
+    displayMusic(music);
+    playMusic()
+}
+
+function nextMusic() {
+    player.next();
+    let music = player.getMusic();
+    displayMusic(music);
+    playMusic();
+}
+
+
+function pauseMusic() {
+    audio.pause();
+    play.classList = "fa-solid fa-play"
+};
+
+function playMusic() {
+    audio.play();
+    play.classList = "fa-solid fa-pause"
+};
+
+function togglePlay() {
+    if (container.classList.contains("playing")) {
+        container.classList.remove("playing")
+    } else {
+        container.classList.add("playing")
+    }
+}
 
 
