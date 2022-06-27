c6db241bbb14bc9648474a483c23119ed7f476cf
Fatih Kincir <kincirfatih@gmail.com>
2022-06-27 13:49:38 +0200

    music list functionality added

--- app.js
+++ app.js
@@ -10,6 +10,7 @@ const duration = document.querySelector("#duration");
 const progressBar = document.querySelector("#progress-bar");
 const volume = document.querySelector("#volume");
 const volumeBar = document.querySelector("#volume-bar");
+const ul = document.querySelector("#music-list .list-group");
 
 const player = new MusicPlayer(musicList);
 
@@ -17,8 +18,9 @@ const player = new MusicPlayer(musicList);
 
 window.addEventListener("load", () => {
     let music = player.getMusic();
-    //console.log(music.getName())
+
     displayMusic(music);
+    displayMusicList(player.musicList);
 
 });
 
@@ -136,4 +138,27 @@ volume.addEventListener("click", () => {
         audio.volume = 1;
     }
 
-})
+});
+
+const displayMusicList = (list) => {
+    for (let i = 0; i < list.length; i++) {
+        console.log(list[i])
+        let content = `
+        <li class="list-group-item d-flex justify-content-between align-items-center">
+            <span>${list[i].getName()}</span>
+            <span id="music-${i}" class="badge bg-primary rounded-pill"></span>
+            <audio class="music-${i}" src="mp3/${list[i].file}"></audio>
+        </li>
+    `;
+        ul.insertAdjacentHTML("beforeend", content);
+        let liAudioDuration = ul.querySelector(`#music-${i}`);
+        let liAudioTag = ul.querySelector(`.music-${i}`);
+
+        liAudioTag.addEventListener("loadeddata", () => {
+            liAudioDuration.innerText = calculateTime(liAudioTag.duration);
+        })
+
+
+
+    }
+}
